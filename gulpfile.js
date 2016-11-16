209622c8243b50eba7b7a55933574d4e9bb15870
lazamar <lazaronijunior@gmail.com>
2016-11-16 11:59:51 +0000

    Remove browser-sync and setup npm start as server lauhch

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,4 @@
 
 const organiser = require('gulp-organiser');
 organiser.registerAll('./gulp-tasks', {
-  'browser-sync': {
-    src: '.', // it doesn't matter, it's just so the task object is not ignored.
-    reloadOn: ['transpile-react'], // reload page when these tasks happen
-    startPath: 'pages/index.html',
-    baseDir: './demo',
-  },
 });
