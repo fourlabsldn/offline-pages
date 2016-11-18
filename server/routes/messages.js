8580ab6aa20a6a8c5a709f4d82544031df7a3790
lazamar <lazaronijunior@gmail.com>
2016-11-18 15:53:38 +0000

    Make message thread persist to improve demo

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,6 +1,15 @@
 /* eslint-disable camelcase, no-param-reassign */
+const fs = require('fs');
+const path = require('path');
+
+function loadMessages() {
+  const msgFileAddr = path.join(__dirname, 'message-record.json');
+  const msgFileContent = fs.readFileSync(msgFileAddr);
+  const messages = JSON.parse(msgFileContent);
+  return messages || [];
+}
 
 module.exports = (req, res) => {
-  req.app.locals.messages = req.app.locals.messages || [];
+  req.app.locals.messages = loadMessages();
   res.json(req.app.locals.messages);
 };
--- server/routes/new-message.js
+++ server/routes/new-message.js
@@ -1,12 +1,25 @@
 /* eslint-disable camelcase, no-param-reassign */
+const fs = require('fs');
+const path = require('path');
+const msgFileAddr = path.join(__dirname, 'message-record.json');
+
+function loadMessages() {
+  const msgFileContent = fs.readFileSync(msgFileAddr);
+  const messages = JSON.parse(msgFileContent);
+  return messages || [];
+}
+
+function setMessages(msg) {
+  fs.writeFileSync(msgFileAddr, JSON.stringify(msg));
+}
 
 module.exports = (req, res) => {
-  req.app.locals.messages = req.app.locals.messages || [];
-
   console.log('Request body', req.body);
-  const newMessage = req.body.content;
 
-  req.app.locals.messages.push(newMessage);
+  const messages = loadMessages() || [];
+  const newMessage = req.body.content;
+  const newMessages = messages.concat([newMessage]);
+  setMessages(newMessages);
 
   res.json({ posted: true });
 };
