c594038c9ab13fb12cd94e901a28f32090f51f3a
lazamar <lazaronijunior@gmail.com>
2016-11-24 10:41:57 +0000

    Remove service worker successfull registration message

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -7,10 +7,7 @@ if (window.navigator && 'serviceWorker' in window.navigator) {
   // we have to include it at the root of our website, as it can only
   // control requests to paths under the path it is in.
   window.navigator.serviceWorker.register('/service-worker.js', {scope: '/'})
-    .then(registration => {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(err => {
+    .catch(err => {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
